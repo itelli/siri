035464db294a0538db30f4a6a50b5e67400d22e0
itelli <lhz@itelligence.de>
2013-02-01 12:13:08 +0100

    Update lib/siriproxy-itelli.rb

--- lib/siriproxy-itelli.rb
+++ lib/siriproxy-itelli.rb
@@ -56,20 +56,6 @@ class SiriProxy::Plugin::Itelli < SiriProxy::Plugin
    listen_for /Please (.*) and send (.*) to (.*)/i do | app, obejct, person |
 	say "Find the new Priceliste attached", spoken: "Here is your message to Klaus Rainer Berger"
 	
-        object = SiriAddViews.new
-	object.make_root(last_ref_id)
-	
-	answer = SiriAnswer.new("New message", [SiriAnswerLine.new('logo','http://s7.directupload.net/images/130130/jdchnb9s.png'),
-	SiriAnswerLine.new("Hello Klaus"),
-	SiriAnswerLine.new("Please find the new priceliste attached."),
-	SiriAnswerLine.new(""),
-	SiriAnswerLine.new("Best regards,"),
-	SiriAnswerLine.new("Oliver"),
-	])
-	
-	object.views << SiriAnswerSnippet.new([answer])
-	send_object object 
-	
 	response = ask "Send or Cancel?" 
 	
 	if (response =~ /send/i)
@@ -79,16 +65,17 @@ class SiriProxy::Plugin::Itelli < SiriProxy::Plugin
 	end
 	
 	say "Open Task for Account 1000 in SAP was closed."
+	say "Email was sent."
     request_completed
     end  
     
     listen_for /How can I close (.*)/i do | deals |
-    	say "Sorry I'm Siri not Hana!"  
+    	say "Sorry, I'm Siri not Hana!"  
     request_completed
     end     
     
     listen_for /What a pity (.*)/i do | help |
-    	say "No, problem - don't forget the party tonight!"  
+    	say "No problem - don't forget the party tonight!"  
     request_completed
     end       
 	
